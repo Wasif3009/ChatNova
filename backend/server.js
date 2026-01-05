d6af51a882c3cbc88ce806b198d4c7df7737352a
Wasif3009 <wasifshaikh1233@gmail.com>
2026-01-05 15:56:28 +0530

    commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is run sucess");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(5000, console.log(`server Started on ${PORT}`));
